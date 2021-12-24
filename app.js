e16f4c98ce1524439c78b0e5d3c6394e6f0aa1e3
Mehul <mehulprajapati2802@gmail.com>
2021-12-23 23:25:35 -0500

    restructure the project

--- app.js
+++ app.js
@@ -1,1 +1,15 @@
-console.log('Task Manager App')
+const express = require('express');
+const path = require('path');
+const app = express();
+const port = process.env.PORT || 4000;
+app.use(express.static(__dirname + '/public/'));
+
+
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname + "/views", '/index.html'));
+});
+
+
+app.listen(port, () => {
+    console.log(`Listening on port:${port}`)
+});
