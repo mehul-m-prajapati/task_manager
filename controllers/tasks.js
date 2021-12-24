283931db5f7247a93d4070ecc4e3e5c8a0fc92bc
Mehul <mehulprajapati2802@gmail.com>
2021-12-24 14:48:04 -0500

    added update task controller

--- controllers/tasks.js
+++ controllers/tasks.js
@@ -28,18 +28,43 @@ const getTask = async (req, res) => {
         if (!task) {
             return res.status(404).json({msg: `No task with id: ${taskId}`});
         }
-        res.status(201).json(task);
+        res.status(200).json(task);
     } catch (error) {
         res.status(500).json(error);
     }
 };
 
+const deleteTask = async (req, res) => {
 
-const updateTask = (req, res) => {
-    res.send('update a task');
+    try {
+        const {id: taskId} = req.params;
+        const task = await taskModel.findByIdAndDelete({_id: taskId});
+        if (!task) {
+            return res.status(404).json({msg: `No task with id: ${taskId}`});
+        }
+        //res.status(200).send();
+        res.status(200).json(task);
+    } catch (error) {
+        res.status(500).json(error);
+    }
 };
-const deleteTask = (req, res) => {
-    res.send('delete a task');
+
+const updateTask = async (req, res) => {
+
+    try {
+        const {id: taskId} = req.params;
+        const task = await taskModel.findByIdAndUpdate({_id: taskId}, req.body, {
+            new: true,
+            runValidators: true
+        });
+
+        if (!task) {
+            return res.status(404).json({msg: `No task with id: ${taskId}`});
+        }
+        res.status(200).json(task);
+    } catch (error) {
+        res.status(500).json(error);
+    }
 };
 
 
