1cb75b98e966758e6e1bc0ad8836cb65824c6c92
Mehul <mehulprajapati2802@gmail.com>
2021-12-23 23:39:16 -0500

    added /v1/tasks/ route

--- app.js
+++ app.js
@@ -2,14 +2,21 @@ const express = require('express');
 const path = require('path');
 const app = express();
 const port = process.env.PORT || 4000;
+const tasks = require('./routes/tasks');
+
 app.use(express.static(__dirname + '/public/'));
 
+// middlewares
+app.use(express.json()); // to have data in req.body
 
+// routes
 app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname + "/views", '/index.html'));
 });
 
+app.use('/api/v1/tasks', tasks);
 
+// Start the web server
 app.listen(port, () => {
     console.log(`Listening on port:${port}`)
 });
